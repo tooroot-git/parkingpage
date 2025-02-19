0b81381b716aeaecc9634431b888c95b2a023f7d
tooroot-git <cornpopcorn@duck.com>
2025-02-19 03:16:47 -0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
+/** @type {import('next').NextConfig} */
 const CompressionPlugin = require('compression-webpack-plugin');
 
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
   experimental: {
@@ -12,26 +12,30 @@ const nextConfig = {
   },
   webpack: (config, { dev, isServer }) => {
     if (!dev && !isServer) {
+      // דחיסת קבצים עם Brotli
       config.plugins.push(
         new CompressionPlugin({
-          algorithm: 'brotliCompress', // ברירת מחדל: gzip. אבל Brotli עדיף מבחינת דחיסה
+          algorithm: 'brotliCompress',
           filename: '[path][base].br',
           test: /\.(js|css|html|svg)$/,
-          threshold: 10240, // דוחס רק קבצים מעל 10KB
-          minRatio: 0.7, // היחס בין הקובץ לפני ואחרי הדחיסה
+          threshold: 10240,
+          minRatio: 0.7,
         })
       );
+
+      // הסרת Source Maps מה-Client כדי לחסוך מקום
+      config.devtool = false;
+
+      // ✅ החרגת חבילות כבדות מלהיכנס ל-Client
+      config.externals = [...config.externals, 'moment', 'axios', 'lodash'];
     }
 
+    // הגבלת גודל כל Chunk
     config.optimization.splitChunks = {
       chunks: 'all',
-      maxSize: 20000000, // מגביל כל צ'אנק ל-20MB
+      maxSize: 20000000, // הגבלת כל Chunk ל-20MB כדי לא לעבור את מגבלת Cloudflare
       minSize: 50000,
     };
-
-    if (!dev) {
-      config.devtool = false; // ביטול Sourcemaps בפרודקשן
-    }
 
     return config;
   },
